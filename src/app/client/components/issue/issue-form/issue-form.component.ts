099af9b1534e4283b0148ea9351ec0f4dc4d236d
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-05-04 12:16:46 +0400

    add service type field

--- src/app/client/components/issue/issue-form/issue-form.component.ts
+++ src/app/client/components/issue/issue-form/issue-form.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Issue} from '../../../data/model/issue.model';
+import {ServiceType} from '../../../../core/data/model/service-type.model';
+import {Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {State} from '../../../../app.state';
+import {ServiceTypeListLoadStart} from '../../../../core/data/service-type.actions';
 
 @Component({
   selector: 'app-issue-form',
@@ -12,9 +17,14 @@ export class IssueFormComponent implements OnInit {
 
   @Input() errors = {};
 
-  constructor() { }
+  serviceTypes: Observable<Array<ServiceType>>;
+
+  constructor(private store: Store<State>) { }
 
   ngOnInit() {
+    this.serviceTypes = this.store.pipe(select(state => state.serviceType.list));
+
+    this.store.dispatch(new ServiceTypeListLoadStart());
   }
 
   onSubmitHandler(event)
@@ -25,5 +35,15 @@ export class IssueFormComponent implements OnInit {
   onAddressUpdateHandler(event)
   {
     this.issue.company = null;
+  }
+
+  compareServiceTypes(a: ServiceType, b: ServiceType)
+  {
+    if (!a || !b)
+    {
+      return false;
+    }
+
+    return a.id === b.id;
   }
 }
