c0dab16c14eec6192ca568837b4b344ba9df9084
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-06-02 16:45:49 +0400

    add common header at the security module pages

---
+++ src/app/security/components/common-header/common-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-security-common-header',
+  templateUrl: './common-header.component.html',
+  styleUrls: ['./common-header.component.css']
+})
+export class CommonHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -10,6 +10,7 @@ import { SecurityRoutingModule } from './security-routing.module';
 import { RegisterSuccessfulPageComponent } from './components/register-successful-page/register-successful-page.component';
 import { PasswordRecoveryRequestPageComponent } from './components/password-recovery-request-page/password-recovery-request-page.component';
 import { PasswordResetPageComponent } from './components/password-reset-page/password-reset-page.component';
+import { CommonHeaderComponent } from './components/common-header/common-header.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PasswordResetPageComponent } from './components/password-reset-page/pas
     ConfirmationPageComponent,
     RegisterSuccessfulPageComponent,
     PasswordRecoveryRequestPageComponent,
-    PasswordResetPageComponent
+    PasswordResetPageComponent,
+    CommonHeaderComponent
   ],
     imports: [
         CommonModule,
